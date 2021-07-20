e3a90f98ef0a101c61311c5db0279097bdae5c07
Gracie <m.graciemcguire@gmail.com>
2021-07-20 10:38:23 -0700

    Import components into App for test clarity

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Article from './Article'
+import Header from './Header'
 
 function App() {
-  return <div className="App">learn react!</div>;
+  return <div className="App">
+    learn react!
+    <Header/>
+    <Article />
+  </div>;
 }
 
 export default App;
