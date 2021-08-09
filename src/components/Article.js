a6a97000f36e0ab34a16a73a8c4beee08b720a37
Liz Burton <liz.burton147@gmail.com>
2021-08-09 11:03:29 -0400

    Edited starter code to better match instructions

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,7 +3,6 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* uncomment the line below to pass the test */}
       {/* please pass this test */}
     </div>
   );
