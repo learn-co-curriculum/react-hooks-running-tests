09301dedb4feb36cf1fc858d2ceae88436db6178
Collins <mutaic84@gmail.com>
2022-04-26 09:34:02 +0300

    commit tree 0360547a82bff64e631bdba9f9d1526336cfb630

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header(props) {
       return (
         <h1>
           {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
+          { "hello from the Header!" }
         </h1>
       )
 }
