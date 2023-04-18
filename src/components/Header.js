1193f59dfa3448fb9076980182a6ab03b68f1843
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-04-18 22:24:41 +0300

    commit tree 61247275081c1de9563c3dc74e6261a016118c58

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import React from "react";
 function Header(props) {
       return (
         <h1>
-          {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
+          
+         hello from the Header!
         </h1>
       )
 }
