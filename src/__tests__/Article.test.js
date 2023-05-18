d27257a3d54bb7cc9985d3d8353baea5cf467d11
Brandon Wallace <brandonwallace403@gmail.com>
2023-05-18 17:14:56 -0400

    adds changes

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,10 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
 });
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,7 @@ import React from "react";
 
 function Header(props) {
       return (
-        <h1>
-          {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
-        </h1>
+         <h1>hello from the Header!</h1>
       )
 }
 
