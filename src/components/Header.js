00c0b61c7a01a44c508857d7c1c7635bbff547e9
hayderali1 <127198671+hayderali1@users.noreply.github.com>
2023-06-02 02:38:57 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,7 @@
 import React from "react";
 
 function Header(props) {
-      return (
-        <h1>
-          {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
-        </h1>
-      )
+  return <h1>hello from the Header!</h1>;
 }
 
 export default Header;
