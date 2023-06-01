79888aee18dc3640c395674a9e89e910abaaaee1
hayderali1 <127198671+hayderali1@users.noreply.github.com>
2023-06-02 02:39:58 +0300

    Update Article.test.js

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,9 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import React from "react";
-
-import Article from "../components/Article";
-
+// src/__tests__/Article.test.js
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
+
+  // add this line
+  screen.debug();
 
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
